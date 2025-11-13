08015bd2c34cfeb298bf53838b0a12156bc75a3a
Mahmoud Zahran <mi4654110@gmail.com>
2025-11-13 20:52:32 +0200

    Question Module : Add Question&Answer Tables

---
+++ SurveyBasket/Entities/Answer.cs
@@ -0,0 +1,10 @@
+﻿namespace SurveyBasket.Entities;
+
+public class Answer
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public int QuestionId { get; set; }
+    public Question Question { get; set; } = default!;
+}
--- SurveyBasket/Persistence/ApplicationDbContext.cs
+++ SurveyBasket/Persistence/ApplicationDbContext.cs
@@ -10,10 +10,21 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
 
     public DbSet<Poll> Polls { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Answer> Answers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        var cascadeFKs = modelBuilder.Model
+                .GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => fk.DeleteBehavior == DeleteBehavior.Cascade && !fk.IsOwnership);
+
+        foreach (var fk in cascadeFKs)
+            fk.DeleteBehavior = DeleteBehavior.Restrict;
+
 
         base.OnModelCreating(modelBuilder);
     }

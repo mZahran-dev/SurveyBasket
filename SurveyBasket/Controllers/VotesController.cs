0fbe72332090ec699b6321e7efe2f518a5e6f348
Mahmoud Zahran <mi4654110@gmail.com>
2025-11-19 21:17:20 +0200

    Votes Module : Add StartVote endPoint Add GetAvailable Votes Method

---
+++ SurveyBasket/Controllers/VotesController.cs
@@ -0,0 +1,28 @@
+﻿    using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using SurveyBasket.Services.Questions;
+using System.Security.Claims;
+
+namespace SurveyBasket.Controllers;
+
+[Route("api/polls/{pollId}/vote/[controller]")]
+[ApiController]
+public class VotesController(IQuestionService questionService) : ControllerBase
+{
+    private readonly IQuestionService _questionService = questionService;
+
+    [HttpGet("")]
+    public async Task<IActionResult> Start(int pollId, CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await _questionService.GetAvailableAsync(pollId, userId!, cancellationToken);
+
+        if(result.IsSuccess)
+            return Ok(result.Value);
+
+        return result.Error.Equals(VoteErrors.DublicatedVote)
+            ? result.ToProblem()
+            : result.ToProblem();
+    }
+}
---
+++ SurveyBasket/Errors/VoteErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace SurveyBasket.Errors;
+
+public static class VoteErrors
+{
+    //public static readonly Error VoteNotFound =
+    //    new Error("poll.NotFound", "no Poll was Found for the given ID", StatusCodes.Status404NotFound);
+
+    public static readonly Error DublicatedVote =
+        new Error("Vote.DublicatedVote", "User Already Voted", StatusCodes.Status409Conflict);
+}
+
